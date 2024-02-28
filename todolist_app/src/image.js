ec19c78e5f13cc990ab15de884d7fca8127e7984
Kaushik1210 <kabikaushik1210@gmail.com>
2024-02-28 18:12:26 +0530

    first commit

---
+++ todolist_app/src/image.js
@@ -0,0 +1,17 @@
+import bgdesktopdark from './assets/bgdesktopdark.jpg'
+import iconsun from './assets/iconsun.svg'
+import iconmoon from './assets/iconmoon.svg'
+import iconcheck from './assets/iconcheck.svg'
+import bgdesktoplight from './assets/bgdesktoplight.jpg'
+import iconcross from './assets/iconcross.svg'
+
+
+export default{
+    bgdesktopdark,
+    iconsun,
+    iconcheck,
+    bgdesktoplight,
+    iconmoon,
+    iconcross
+
+}
